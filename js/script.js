99c759e5b328c33bac8742c91c98b71488307135
Roberto Robles <po.usa.2029@gmail.com>
2026-03-09 15:39:32 -0400

    adding quotes slideshow

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // Scroll Reveal Animation
+
 function revealOnScroll() {
   const reveals = document.querySelectorAll(".reveal");
 
@@ -22,3 +23,29 @@ toggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
+
+// Quotes Slideshow
+
+var slideIndex = 0;
+
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("quotes");
+
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > x.length) {
+	slideIndex = 1
+  }
+
+  x[slideIndex-1].style.display = "block";
+
+  setTimeout(carousel, 3000);
+
+}
